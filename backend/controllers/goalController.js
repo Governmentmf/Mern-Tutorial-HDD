526318f7c8c8a477d8770e425b83a2da95bd6b2f
Justin Edwards <highdefdesignsinc@gmail.com>
2022-09-25 09:40:53 -0500

    Prepare deploy

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
 const User = require('../models/userModel')
 
-// @DESC    Get Goals
+// @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
@@ -12,7 +12,7 @@ const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json(goals)
 })
 
-// @DESC    Set Goal
+// @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
 const setGoal = asyncHandler(async (req, res) => {
@@ -25,10 +25,11 @@ const setGoal = asyncHandler(async (req, res) => {
     text: req.body.text,
     user: req.user.id,
   })
+
   res.status(200).json(goal)
 })
 
-// @DESC    UPDATE Goal
+// @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
@@ -38,24 +39,27 @@ const updateGoal = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Goal not found')
   }
-  const user = await User.findById(req.user.id)
+
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
-  // Make sure the login user matches goal user
-  if (goal.user.toString() !== user.id) {
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('User not authorized')
   }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
+
   res.status(200).json(updatedGoal)
 })
 
-// @DESC    Delete Goals
+// @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
@@ -65,14 +69,15 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Goal not found')
   }
-  const user = await User.findById(req.user.id)
+
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
-  // Make sure the login user matches goal user
-  if (goal.user.toString() !== user.id) {
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('User not authorized')
   }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,59 @@
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import GoalForm from '../componets/GoalForm'
+import GoalItem from '../componets/GoalItem'
+import Spinner from '../componets/Spinner'
+import {getGoals, reset} from '../features/goals/goalSlice'
+
+
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  
+  const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
+
+  useEffect(() => {
+    if(isError){
+      console.log(message);
+    }
+
+    if(!user){
+      navigate('/login')
+    }
+
+    dispatch(getGoals())
+    
+    return () => {
+      dispatch(reset())
+    }
+
+  },[user, navigate, isError, message, dispatch])
+
+  if(isLoading) {
+    return <Spinner/>
+  }
+
+  return(
+  <>
+    <section className="heading">
+      Welcome {user && user.name}
+      <p>Goals Dashboard</p>
+    </section>
+
+    <GoalForm/>
+
+    <section className="content">
+         {goals.length > 0 ? (<div className="goals">
+          {goals.map((goal) => (
+            <GoalItem key={goal._id} goal={goal}/>
+          ))}
+         </div>
+         ) : (
+         <h3>Why havent you set any goals you BUM!</h3>)}
+    </section> 
+  </>
   )
 }
 
